6edb01d6930f0f0153b40f240d757679317de01a
Atnora <140785891+Atnora@users.noreply.github.com>
2026-04-08 17:18:45 +0700

    Change timestamp format for CSV filename

--- app.py
+++ app.py
@@ -134,8 +134,8 @@ if uploaded_file:
 
   
     csv = df_display.to_csv(index=False).encode('utf-8')
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
-    file_name = f"hasil_validasi_{timestamp}.csv"
+    tanggal = datetime.now().strftime("%Y-%m-%d")
+    file_name = f"hasil_validasi_{tanggal}.csv"
 
     st.download_button(
     label="⬇️ Download Hasil CSV",
